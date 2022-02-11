d950e5bc56f3e4c3295f5f1fed6e0f53ec839837
seshan-b <seshan.b@gmail.com>
2022-02-11 22:49:55 +1300

    Completed a React Todo List

--- todo-list-in-react/src/App.js
+++ todo-list-in-react/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
+// Import all Hooks
+import React,  {useState, useEffect, useRef} from 'react'
+
+
+// Main Component 
 function App() {
+
+  // Binding reference hook
+  const todoText = useRef()
+
+  const [todos,  setTodos] = useState([])
+
+  // Side Effects Lifecycle
+  useEffect(() => {
+    const existingTodos = localStorage.getItem('todos')
+    setTodos(existingTodos ? JSON.parse(existingTodos) : [])
+
+  }, []) 
+
+
+  // Events
+  function addTodo(event) {
+    event.preventDefault()
+    const next = [...todos, todoText.current.value ]
+    setTodos(next)
+    localStorage.setItem('todos', JSON.stringify(next))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))}
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input ref={todoText} type="text" placeholder="What needs to be done?" />
+        <input type="submit" value="Add Todo" />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
